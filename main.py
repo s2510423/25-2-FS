9c5c711a24e7532b9794f3eb8471e0257f91bde3
s2510423 <s2510423@outlook.kr>
2026-01-05 04:17:04 +0000

    feat: re-add main.py for overall process

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
-import plotter
+import nplot
+import BSDT
 
-def parce_all():
-    plotter.parce('noise.txt')
-    plotter.parce('real..txt')
-
-def process_all():
-    plotter.unoffset('noise','EMF.xlsx')
-    plotter.unoffset('real','EMF.xlsx')
-    plotter.true_voltage('noise','unoffset.xlsx')
-    plotter.true_voltage('real','unoffset.xlsx')
-
-def ma(num=100):
-    plotter.moving_average('real','trueV.xlsx',num)
-    plotter.moving_average('noise','trueV.xlsx',num)
-
-def slicer(num=1000):
-    plotter.slicer('real','trueV.xlsx',num)
-    plotter.slicer('noise','trueV.xlsx',num)
-def print_plt(file='trueV'):
-    plotter.plot_2d_time('noise',f'{file}.xlsx','noise')
-    plotter.plot_2d_time('real',f'{file}.xlsx','real')
-
+def launch():
+    for i in BSDT.target:
+        name = i.split('/')[-1]
+        nplot.data(name)
     
+def MakeAll():
+    for i in nplot.datalist:
+        i.make()
+
+def ProcessAll():
+    for i in nplot.datalist:
+        i.process()
+
+
+if __name__ == "__main__":
+    launch()
+    MakeAll()
+    ProcessAll()
+    print('시팔 살려주세요') 
